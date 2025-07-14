9ac90b2356c7fe71381088eaeb808e808a225d12
eskainah <eskainah@gmail.com>
2025-07-14 10:39:20 +0000

    update store model

--- stores/models.py
+++ stores/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-from customers.models import User  # Assuming User model is in the 'customer' app
+from customers.models import User 
 from customers.models import Client
-# class Store(models.Model):
-#     tenant = models.ForeignKey(Client, on_delete=models.CASCADE) 
-#     store_name = models.CharField(max_length=100)
-#     location = models.CharField(max_length=255)
-    
-#     def __str__(self):
-#         return self.store_name
+
 
 class Store(models.Model):
     tenant = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -19,6 +13,7 @@ class Store(models.Model):
 
     def __str__(self):
         return self.store_name
+
 class Employee(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='employees')
     store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='employees')
