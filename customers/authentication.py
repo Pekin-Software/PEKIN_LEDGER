65d88b2880cf86acc59c3063be3696df76aefb5d
eskainah <eskainah@gmail.com>
2025-09-08 12:52:37 +0000

    Update Settings for S3 and AWS deploment

--- customers/authentication.py
+++ customers/authentication.py
@@ -5,43 +5,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# class TenantAwareJWTAuthentication(JWTAuthentication):
-#     def authenticate(self, request):
-#         # First authenticate with JWT token
-#         if request.path.endswith("/logout/"):
-#             return super().authenticate(request)
-        
-#         raw_auth = super().authenticate(request)
-
-#         if raw_auth is None:
-#             return None  # No credentials provided
-
-#         user, validated_token = raw_auth
-
-#         # Tenant info from token (probably tenant ID or slug)
-#         token_tenant = validated_token.get("tenant")
-#         if not token_tenant:
-#             raise AuthenticationFailed("Token missing tenant info.")
-
-#         # Extract domain from request
-#         host = request.get_host().split(':')[0]  # Remove port if any
-#         # Try to find Domain object matching this host
-#         try:
-#             domain_obj = Domain.objects.get(domain=host)
-#         except Domain.DoesNotExist:
-#             logger.error(f"Domain object not found for host: {host}")
-#             raise PermissionDenied("Request domain not recognized.")
-
-#         request_tenant_id = str(domain_obj.tenant.id)
-
-#         # Compare tenant from token with tenant of the request domain
-#         if str(token_tenant) != request_tenant_id:
-#             logger.warning(f"Tenant mismatch! Token tenant: {token_tenant}, Request tenant: {request_tenant_id}")
-#             raise PermissionDenied("Tenant mismatch between token and request domain.")
-
-#         # All good
-#         return (user, validated_token)
-
 class TenantAwareJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         # For logout, fallback to default
