6384714dc51d1c20b812383df5d18e24ccd9747a
eskainah <eskainah@gmail.com>
2025-09-11 10:29:38 +0000

    update templates settings

--- ledger_api/settings.py
+++ ledger_api/settings.py
@@ -324,6 +324,21 @@ WSGI_APPLICATION = "ledger_api.wsgi.application"
 # ------------------------------------------------------------------------------
 # DATABASE
 # ------------------------------------------------------------------------------
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # optional, but recommended
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 DATABASES = {
     "default": dj_database_url.config(
